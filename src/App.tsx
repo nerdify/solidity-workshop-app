6d565a2f6c466b82d5c402372644e78ccdaf7c83
Hosmel Quintana <hosmelq@gmail.com>
2021-11-03 17:41:48 -0600

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,89 @@
-import {ChakraProvider} from '@chakra-ui/react'
+import {useEffect, useState} from 'react'
+
+import {Alert, Button, ChakraProvider, Container, Stack} from '@chakra-ui/react'
 
 import {MessageList} from 'components'
 import theme from 'theme'
 
 export function App() {
+  const [isEthereumEnabled, setIsEthereumEnabled] = useState(false)
+  const [isWalletConnected, setIsWalletConnected] = useState(false)
+
+  useEffect(() => {
+    // @ts-expect-error
+    if (typeof window.ethereum !== 'undefined') {
+      setIsEthereumEnabled(true)
+    }
+  }, [])
+
+  useEffect(() => {
+    if (!isEthereumEnabled) {
+      return
+    }
+
+    async function getAccount() {
+      // @ts-expect-error
+      const accounts = await window.ethereum.request({method: 'eth_accounts'})
+
+      if (accounts.length > 0) {
+        setIsWalletConnected(true)
+      }
+    }
+
+    getAccount()
+  }, [isEthereumEnabled])
+
+  useEffect(() => {
+    if (!isEthereumEnabled) {
+      return
+    }
+
+    function handleAccountsChanged(accounts: string[]) {
+      setIsWalletConnected(accounts.length > 0)
+    }
+
+    // @ts-expect-error
+    window.ethereum.on('accountsChanged', handleAccountsChanged)
+
+    return () => {
+      // @ts-expect-error
+      window.ethereum.removeListener('accountsChanged', handleAccountsChanged)
+    }
+  }, [isEthereumEnabled])
+
   return (
     <ChakraProvider theme={theme}>
-      <MessageList />
+      <Container paddingBlockStart="4">
+        <Stack spacing="4">
+          {!isEthereumEnabled && (
+            <Alert borderRadius="base" status="warning">
+              Please enable your MetaMask wallet to use this app.
+            </Alert>
+          )}
+          {!isWalletConnected && (
+            <Button colorScheme="green" onClick={handleConnect}>
+              Connect to MetaMask
+            </Button>
+          )}
+          <MessageList isWalletConnected={isWalletConnected} />
+        </Stack>
+      </Container>
     </ChakraProvider>
   )
+
+  function handleConnect() {
+    async function requestAccounts() {
+      // const accounts =
+      // @ts-expect-error
+      await window.ethereum.request({
+        method: 'eth_requestAccounts',
+      })
+
+      // if (accounts.length > 0) {
+      //   setIsWalletConnected(true)
+      // }
+    }
+
+    requestAccounts()
+  }
 }
--- src/components/MessageList/MessageList.tsx
+++ src/components/MessageList/MessageList.tsx
@@ -1,42 +1,74 @@
+import {ethers} from 'ethers'
 import {FastField, Form, Formik, FormikHelpers} from 'formik'
 
-import {Button, Container, Stack, Textarea} from '@chakra-ui/react'
+import {Button, Stack, Textarea} from '@chakra-ui/react'
+
+import ChatJSON from 'Chat.json'
+
+interface MessageListProps {
+  isWalletConnected: boolean
+}
 
 interface Values {
   message: string
 }
 
-export function MessageList() {
+export function MessageList({isWalletConnected}: MessageListProps) {
   return (
-    <Container>
-      <Formik
-        initialValues={{
-          message: '',
-        }}
-        onSubmit={handleSubmit}
+    <Formik
+      initialValues={{
+        message: '',
+      }}
+      onSubmit={handleSubmit}
+    >
+      <Stack
+        as={Form}
+        bgColor="white"
+        borderRadius="base"
+        boxShadow="base"
+        padding="4"
+        spacing="4"
       >
-        <Stack
-          as={Form}
-          bgColor="white"
-          borderRadius="base"
-          boxShadow="base"
-          padding="4"
-          spacing="4"
-        >
-          <FastField name="message">
-            {({field}) => (
-              <Textarea {...field} placeholder="Type a message..." />
-            )}
-          </FastField>
-          <Button colorScheme="blue" type="submit">
-            Send Message
-          </Button>
-        </Stack>
-      </Formik>
-    </Container>
+        <FastField name="message">
+          {({field}) => (
+            <Textarea
+              {...field}
+              disabled={!isWalletConnected}
+              placeholder="Type a message..."
+            />
+          )}
+        </FastField>
+        <Button colorScheme="blue" disabled={!isWalletConnected} type="submit">
+          Send Message
+        </Button>
+      </Stack>
+    </Formik>
   )
 
-  function handleSubmit(values: Values, formikBag: FormikHelpers<Values>) {
+  async function handleSubmit(
+    values: Values,
+    formikBag: FormikHelpers<Values>
+  ) {
     console.log(`values: `, values)
+    // @ts-expect-error
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    const signer = provider.getSigner()
+    const contract = new ethers.Contract(
+      process.env.REACT_APP_CONTRACT_ADDRESS as string,
+      ChatJSON.abi,
+      signer
+    )
+
+    const txn = await contract['createMessage(string,string)'](
+      values.message,
+      'hosmelq'
+    )
+
+    await txn.wait()
+
+    const messages = await contract.getMessages()
+    console.log(`messages: `, messages)
+
+    formikBag.resetForm()
   }
 }
